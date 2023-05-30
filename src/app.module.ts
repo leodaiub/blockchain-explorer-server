06f06b306b53ca4abc4319fa42bc845fc37d7760
Leonardo Daiub <leodaiub@gmail.com>
2023-05-30 18:25:33 +0200

    feat: cleanup and created  required modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AddressesModule } from './addresses/addresses.module';
+import { TransactionsModule } from './transactions/transactions.module';
+import { SearchHistoryModule } from './search-history/search-history.module';
 
 @Module({
-  imports: [],
+  imports: [AddressesModule, TransactionsModule, SearchHistoryModule],
   controllers: [],
   providers: [],
 })
---
+++ src/search-history/search-history.controller.ts
@@ -0,0 +1,7 @@
+import { Controller } from '@nestjs/common';
+import { SearchHistoryService } from './search-history.service';
+
+@Controller('search-history')
+export class SearchHistoryController {
+  constructor(private readonly searchHistoryService: SearchHistoryService) {}
+}
